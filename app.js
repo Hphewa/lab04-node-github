e7a3634b398e2feaec2ed1fd877970642e355810
Himanga Piyumadi <hphewa2@gmail.com>
2026-02-22 07:37:14 +0530

    Create a web server in app.js

--- app.js
+++ app.js
@@ -15,3 +15,14 @@ fs1.writeFile('file.txt', 'Hello World!', function (err) {
   if (err) throw err;
   console.log('File saved!');
 });
+
+//Creating a web server
+const http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('Hello World!');
+  res.end();
+}).listen(3000);
+
+console.log("Server running at http://localhost:3000");
