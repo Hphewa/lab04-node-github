25dd9e7173d3e94cb2decbaab7b4085f5a4fa9d3
Himanga Piyumadi <hphewa2@gmail.com>
2026-02-22 07:25:47 +0530

    Write in the file.txt using app.js

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 console.log("Hello from Node.js!");
 
+// Read file
 const fs = require('fs');
 
 fs.readFile('file.txt', 'utf8', function (err, data) {
   if (err) throw err;
   console.log(data);
+});
+
+//Write to file
+const fs1 = require('fs');
+
+fs1.writeFile('file.txt', 'Hello World!', function (err) {
+  if (err) throw err;
+  console.log('File saved!');
 });
